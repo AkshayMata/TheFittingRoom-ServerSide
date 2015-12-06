156cb2192c49ecb6e41397d9262d63499de3fe4e
Akshay Mata <akam520@gmail.com>
2015-12-05 22:25:11 -0500

    Add test cases for getProducts and getLikedProducts

--- FlaskApp/test__init__.py
+++ FlaskApp/test__init__.py
@@ -59,10 +59,56 @@ def test_authenticate_11():
 	assert authenticate("TommyChen", "TommyChen") == "Invalid User/Password Combination"
 
 def test_authenticate_12():
-	assert authenticate("TommyChen", "Tom!") == "User Not Registered"
+	assert authenticate("TommyC", "Tom!") == "User Not Registered"
 
+def test_getProducts_1():
+	assert getProducts("AkshayMata", "5650dfdf760a441b7e2a765e") != "Invalid user/token combination"
 
-db = connect()
+def test_getProducts_2():
+	assert getProducts("AkshayMata", "5650dfdf760a441b7e2a765") == "Invalid user/token combination"
+
+def test_getProducts_3():
+	assert getProducts("CyrilGeorge", "5650dfdf760a441b7e2a7661") != "Invalid user/token combination"
+
+def test_getProducts_4():
+	assert getProducts("CyrilGeorge", "5650dfdf760a441b7e2a765e") == "Invalid user/token combination"
+
+def test_getProducts_5():
+	assert getProducts("SamiModak", "5650dfdf760a441b7e2a765f") != "Invalid user/token combination"
+
+def test_getProducts_6():
+	assert getProducts("SamiModak", "5650dfdf760a441b7e2a765e") == "Invalid user/token combination"
+
+def test_getProducts_7():
+	assert getProducts("TommyChen", "5650dfdf760a441b7e2a7660") != "Invalid user/token combination"
+
+def test_getProducts_8():
+	assert getProducts("TommyChen", "5650dfdf760a441b7e2a765e") == "Invalid user/token combination"
+
+def test_getLikedProducts_1():
+	assert getLikedProducts("AkshayMata", "5650dfdf760a441b7e2a765e") != "Invalid user/token combination"
+
+def test_getLikedProducts_2():
+	assert getLikedProducts("AkshayMata", "5650dfdf760a441b7e2a765") == "Invalid user/token combination"
+
+def test_getLikedProducts_3():
+	assert getLikedProducts("CyrilGeorge", "5650dfdf760a441b7e2a7661") != "Invalid user/token combination"
+
+def test_getLikedProducts_4():
+	assert getLikedProducts("CyrilGeorge", "5650dfdf760a441b7e2a765e") == "Invalid user/token combination"
+
+def test_getLikedProducts_5():
+	assert getLikedProducts("SamiModak", "5650dfdf760a441b7e2a765f") != "Invalid user/token combination"
+
+def test_getLikedProducts_6():
+	assert getLikedProducts("SamiModak", "5650dfdf760a441b7e2a765e") == "Invalid user/token combination"
+
+def test_getLikedProducts_7():
+	assert getLikedProducts("TommyChen", "5650dfdf760a441b7e2a7660") != "Invalid user/token combination"
+
+def test_getLikedProducts_8():
+	assert getLikedProducts("TommyChen", "5650dfdf760a441b7e2a765e") == "Invalid user/token combination"
+
 
 if __name__ == "__main__":
 	nose.runmodule()
